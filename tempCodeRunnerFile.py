6b5d3a7eb9a203b7ccf364e14b2857ab4417f9bb
avnendrapandey <avnindrapandey85@gmail.com>
2025-09-27 15:03:04 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-@app.route("/index",methods=['GET','POST'])
-def index():
-    if request.method=='POST':
-        name=request.form['name']
-        return f'hello{name}!'
-    return render_template('index.html')
